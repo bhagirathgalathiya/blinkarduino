4f972e35112602ca1661a65ae0031537dd69ed1f
bhagirathgalathiya <59164325+bhagirathgalathiya@users.noreply.github.com>
2020-10-01 00:18:08 +0530

    Update current.ino

--- current.ino
+++ current.ino
@@ -1,25 +1,66 @@
-double Voltage = 0;
-double Current = 0;
-void setup(){
-Serial.begin(9600);
+#include "HX711.h"  //You must have this library in your arduino library folder
+ 
+#define DOUT  D2
+#define CLK  D3
+ 
+HX711 scale(DOUT, CLK);
+ 
+//Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
+float calibration_factor = -109525; //-106600 worked for my 40Kg max scale setup 
+ 
+//=============================================================================================
+//                         SETUP
+//=============================================================================================
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 Calibration");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press a,s,d,f to increase calibration factor by 10,100,1000,10000 respectively");
+  Serial.println("Press z,x,c,v to decrease calibration factor by 10,100,1000,10000 respectively");
+  Serial.println("Press t for tare");
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0
+ 
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
 }
-
-void loop(){
-
-// Voltage is Sensed 1000 Times for precision
-for(int i = 0; i < 1000; i++) {
-Voltage = (Voltage + (.0049 * analogRead(A0))); // (5 V / 1024 (Analog) = 0.0049) which converter Measured analog input voltage to 5 V Range
-delay(1);
-}
-Voltage = Voltage /1000;
-
-Current = (Voltage -2.5)/ 0.185; // Sensed voltage is converter to current
-
-Serial.print(“\n Voltage Sensed (V) = “); // shows the measured voltage
-Serial.print(Voltage,2); // the ‘2’ after voltage allows you to display 2 digits after decimal point
-Serial.print(“\t Current (A) = “); // shows the voltage measured
-Serial.print(Current,2); // the ‘2’ after voltage allows you to display 2 digits after decimal point
-
-delay(1000);
-
+ 
+//=============================================================================================
+//                         LOOP
+//=============================================================================================
+void loop() {
+ 
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+ 
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 3);
+  Serial.print(" kg"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+ 
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
+    else if(temp == 's')
+      calibration_factor += 100;  
+    else if(temp == 'x')
+      calibration_factor -= 100;  
+    else if(temp == 'd')
+      calibration_factor += 1000;  
+    else if(temp == 'c')
+      calibration_factor -= 1000;
+    else if(temp == 'f')
+      calibration_factor += 10000;  
+    else if(temp == 'v')
+      calibration_factor -= 10000;  
+    else if(temp == 't')
+      scale.tare();  //Reset the scale to zero
+  }
 }
